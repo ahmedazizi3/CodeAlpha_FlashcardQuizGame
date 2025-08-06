358ba1438a31e72620f3c500f29827d30fc52a7d
Ahmed Azizi <aziziahmed027@gmail.com>
2025-08-06 21:14:01 +0100

    feat: Display HomeScreen in MainActivity
    This commit updates `MainActivity` to display the `HomeScreen` composable.
    
    The `HomeScreen` is now the content of the `Surface` within the `FlashTheme`.

--- app/src/main/java/azizi/ahmed/flash/MainActivity.kt
+++ app/src/main/java/azizi/ahmed/flash/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import azizi.ahmed.flash.packages.screens.HomeScreen
 import azizi.ahmed.flash.ui.theme.FlashTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +25,9 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {  }
+                ) {
+                    HomeScreen()
+                }
             }
         }
     }

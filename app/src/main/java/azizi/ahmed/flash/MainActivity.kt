90152b1b1786bcbb68df006580f02206e8e24b4c
Ahmed Azizi <aziziahmed027@gmail.com>
2025-08-06 18:53:48 +0100

    Initial commit
    This commit introduces the initial project structure for a new Android application named "Flash".
    
    It includes:
    - Basic Android application setup with a `MainActivity` using Jetpack Compose.
    - Gradle build configuration for the app and project levels.
    - Standard Android resource files (manifest, strings, colors, themes, drawables, mipmaps).
    - Default `.gitignore` files for the project and app module.
    - Android Studio IDE configuration files.
    - Basic unit and instrumented tests.
    - Gradle wrapper files.

---
+++ app/src/main/java/azizi/ahmed/flash/MainActivity.kt
@@ -0,0 +1,31 @@
+package azizi.ahmed.flash
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import azizi.ahmed.flash.ui.theme.FlashTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FlashTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {  }
+            }
+        }
+    }
+}
